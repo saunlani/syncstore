afc7dbcffbbafd42664ae6e997ace3b12795fc8f
saunlani <saunlani@gmail.com>
2019-01-07 14:09:55 -0500

    added angular ui, proxy for angular-spring connectivity and angular service for api call.  admin component displays the data from the api call

--- src/main/java/com/sync/purchase/Main.java
+++ src/main/java/com/sync/purchase/Main.java
@@ -1,8 +1,12 @@
 package com.sync.purchase;
 
+/* these imports and the spring boot application annotation are required for all 
+ * Spring Boot Applications */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// this
 @SpringBootApplication
 public class Main {
 
